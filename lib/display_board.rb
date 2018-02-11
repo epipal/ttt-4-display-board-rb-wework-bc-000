32f2604a0b45371943e2234239e1f2959e59f79c
Elizabeth Pipal <elizabeth.pipal@wework.com>
2018-02-11 22:55:54 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,6 +7,6 @@ def display_board(board)
   -----------
  #{board[3] | #{board[4]} | #{board[5] 
      -----------
- #{board[0] | #{board[1]} | #{board[2] "
+ #{board[6] | #{board[7]} | #{board[8] "
   end
   
