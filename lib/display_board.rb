fe8605f671a247b7f5dbb898428d52b26b0d7bab
Elizabeth Pipal <elizabeth.pipal@wework.com>
2018-02-11 22:58:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,10 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  puts " #{board[0] | #{board[1]} | #{board[2] 
-  -----------
- #{board[3] | #{board[4]} | #{board[5] 
-     -----------
- #{board[6] | #{board[7]} | #{board[8] "
-  end
+  puts " #{board[0] | #{board[1]} | #{board[2] "
+  puts "-----------"
+  puts " #{board[3] | #{board[4]} | #{board[5] "
+  puts "-----------"
+  puts " #{board[6] | #{board[7]} | #{board[8] "
+end
   
